be8167df8faed15adb359086cef84212ac07d33d
MasterQuentus <32037705+MasterQuentus@users.noreply.github.com>
2020-12-27 16:50:08 -0600

    Initial commit

---
+++ src/main/java/com/masterquentus/mythiccraft/MythicCraft.java
@@ -0,0 +1,10 @@
+package com.masterquentus.mythiccraft;
+
+import net.fabricmc.api.ModInitializer;
+
+public class MythicCraft implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
